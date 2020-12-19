8d592b7b1e5305788d0ac603415217f1a3cff65e
Pavel Novikov <imortan@gmail.com>
2020-12-19 23:21:50 +0300

    Trim mrs long branch names

--- report/console/src/jvmMain/kotlin/com/e13mort/gitlab_report/cli/render/ASCIIMergeRequestsRender.kt
+++ report/console/src/jvmMain/kotlin/com/e13mort/gitlab_report/cli/render/ASCIIMergeRequestsRender.kt
@@ -8,20 +8,37 @@ import java.text.DateFormat
 import java.util.*
 
 class ASCIIMergeRequestsRender : ReportRender<PrintProjectMergeRequestsInteractor.MergeRequestsReport, String> {
+
+    internal object RenderingSettings {
+        const val branchMaxLength = 50
+        val headers = arrayOf("ID", "From", "To", "Created", "State")
+    }
+
     override fun render(value: PrintProjectMergeRequestsInteractor.MergeRequestsReport): String {
         return table {
             cellStyle {
                 border = true
             }
             header {
-                row("ID", "From", "To", "Created", "State")
+                row(*RenderingSettings.headers)
             }
             runBlocking {
                 value.walk { id: String, sourceBranch: String, targetBranch: String, created: Long, state: String ->
                     val formattedDate = DateFormat.getInstance().format(Date(created))
-                    row(id, sourceBranch, targetBranch, formattedDate, state)
+                    row(
+                        id,
+                        stripBranchName(sourceBranch),
+                        stripBranchName(targetBranch),
+                        formattedDate,
+                        state
+                    )
                 }
             }
         }.toString()
+    }
+
+    private fun stripBranchName(name: String): String {
+        if (name.length <= RenderingSettings.branchMaxLength) return name
+        return name.substring(0, RenderingSettings.branchMaxLength) + Typography.ellipsis
     }
 }
